794da40bc56c08d57d11926ce2a44e26a5d87457
Alex Miranker <alex@miranker.com>
2025-12-06 18:56:30 -0600

    ssh-agent test

--- hosts/xps/home.nix
+++ hosts/xps/home.nix
@@ -34,7 +34,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    # pkgs.nerd-fonts.jetbrains-mono
+    pkgs.nerd-fonts.jetbrains-mono
 
   ];
 
