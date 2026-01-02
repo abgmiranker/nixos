f1daa8e29eeca7642fbe7d01ff3efb26c5a3ca50
Alex Miranker <alex@miranker.com>
2026-01-01 20:30:39 -0600

    pinned nvidia drivers to specific commit

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-nvidia.url = "github:nixos/nixpkgs/e643668fd71b949c53f8626614b21ff71a07379d";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,7 +14,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-nvidia, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -21,6 +22,10 @@
       modules = [
         ./hosts/xps/configuration.nix
         inputs.home-manager.nixosModules.default
+	{
+	  hardware.nvidia.package = 
+	    nixpkgs-nvidia.legacyPackages.x86_64-linux.linuxPackages_latest.nvidiaPackages.stable;
+	}
       ];
     };
   };
